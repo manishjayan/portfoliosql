db0dd5274ebbbb383e73924abf93c8bd9bb98c80
manishjayan <manishjayan@gmail.com>
2018-10-28 19:02:20 +0530

    nodejs express installed and dependencies added

--- server.js
+++
@@ -1,7 +0,0 @@
-var http=require('http');
-function onrequest(request,response){
-	response.writeHead(200,{'Content-Type':'text/plain'});
-	response.write("Hello world");
-	response.end()
-}
-http.createServer(onrequest).listen(8000);
